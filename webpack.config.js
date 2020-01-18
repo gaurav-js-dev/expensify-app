0744d40ef31e6690296e2a11bd3c40f35fd6ac52
Gaurav Sharma <gaurav.haxor@gmail.com>
2020-01-18 19:26:28 +0530

    Testing

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+
+
+module.exports = (env) => {
+
+    const isProduction = env === "production";
+    const CSSExtract = new ExtractTextPlugin("styles.css");
+
+    console.log("env", env);
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, "public", "dist"),
+            filename: "bundle.js"
+        },
+
+        module: {
+            rules: [{
+                loader: "babel-loader",
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                test: /\.s?css$/,
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: "css-loader",
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            CSSExtract,
+            new webpack.DefinePlugin({
+                "process.env.FIREBASE_API_KEY": JSON.stringify("process.env.FIREBASE_API_KEY"),
+                "process.env.FIREBASE_API_KEY": JSON.stringify("process.env.FIREBASE_API_KEY"),
+                "process.env.FIREBASE_API_KEY": JSON.stringify("process.env.FIREBASE_API_KEY"),
+                "process.env.FIREBASE_API_KEY": JSON.stringify("process.env.FIREBASE_API_KEY"),
+                "process.env.FIREBASE_API_KEY": JSON.stringify("process.env.FIREBASE_API_KEY")
+            })
+        ],
+        devtool: isProduction ? "source-map" : "inline-source-map",
+        devServer: {
+            contentBase: path.join(__dirname, "public"),
+            historyApiFallback: true,
+            publicPath: '/dist/'
+
+        }
+    };
+}
+
