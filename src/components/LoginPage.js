4972b539a888a14cc544c40d54e903ffed5c4951
Gaurav Sharma <gaurav.haxor@gmail.com>
2020-01-27 19:24:51 +0530

    Add CSS and styles

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,8 +3,23 @@ import { connect } from "react-redux";
 import { startLogin } from "../actions/auth"
 
 export const LoginPage = ({ startLogin }) => (
-    <div>
-        <button onClick={startLogin}> Login </button>
+    <div className="box-layout">
+        <div className="box-layout__box">
+            <h1 className="box-layout__title"> Expensify </h1>
+            <p> It's time to get your expenses under control </p>
+            <div className='g-sign-in-button' onClick={startLogin}>
+                <div className="content-wrapper">
+                    <div className='logo-wrapper'>
+                        <img src='https://developers.google.com/identity/images/g-logo.png'></img>
+                    </div>
+                    <span className='text-container'>
+                        <span>Sign in with Google</span>
+                    </span>
+                </div>
+            </div>
+
+
+        </div>
     </div>
 );
 
